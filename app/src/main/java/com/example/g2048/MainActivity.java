365cc4988ba1b0888e4ede113b5fa7e84cf60b85
Maciej <maciek2tr99@gmail.com>
2020-01-05 00:36:34 +0100

    [Add] OnSwipeTouchListener

--- app/src/main/java/com/example/g2048/MainActivity.java
+++ app/src/main/java/com/example/g2048/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.g2048;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View mContentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,23 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
+
+        mContentView = findViewById(R.id.fullscreen_content);
+        mContentView.setOnTouchListener(new com.example.game2048.OnSwipeTouchListener(MainActivity.this) {
+            public void onSwipeTop() {
+                Toast.makeText(MainActivity.this, "top", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeRight() {
+                Toast.makeText(MainActivity.this, "right", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeLeft() {
+                Toast.makeText(MainActivity.this, "left", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeBottom() {
+                Toast.makeText(MainActivity.this, "bottom", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
     }
 }
